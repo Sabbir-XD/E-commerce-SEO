036503c6accd397de924747f70d25eddd77533fe
Sabbir-96 <sabbirislam9404@gmail.com>
2025-08-01 01:35:20 +0600

    fetch the data show the UI and click the view button dynamicly this card details show

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,9 +1,1 @@
-'use client';
 
-import { store } from "@/redux/store";
-import { Provider } from "react-redux";
-
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
-}
